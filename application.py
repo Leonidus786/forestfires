1e03b7cdcca896759457e1fd74221fa98a93f9dc
Leonidus786 <abhi.upadh@gmail.com>
2025-12-03 12:47:38 +0530

    Ready to deploy

--- application.py
+++ application.py
@@ -42,25 +42,72 @@
 #     app.run(host="0.0.0.0")
 
 
+# import pickle
+# from flask import Flask, request, jsonify, render_template
+# import numpy as np
+# import pandas as pd
+# from sklearn.preprocessing import StandardScaler
+
+# # WSGI application object – name MUST be "application"
+# application = Flask(__name__)
+
+# ## import ridge regressor model and standard scaler pickle
+# ridge_model = pickle.load(open('models/ridge.pkl', 'rb'))
+# standard_scaler = pickle.load(open('models/scaler.pkl', 'rb'))
+
+# ## Route for home page
+# @application.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @application.route('/predictdata', methods=['GET', 'POST'])
+# def predict_datapoint():
+#     if request.method == 'POST':
+#         Temperature = float(request.form.get('Temperature'))
+#         RH = float(request.form.get('RH'))
+#         Ws = float(request.form.get('Ws'))
+#         Rain = float(request.form.get('Rain'))
+#         FFMC = float(request.form.get('FFMC'))
+#         DMC = float(request.form.get('DMC'))
+#         ISI = float(request.form.get('ISI'))
+#         Classes = float(request.form.get('Classes'))
+#         Region = float(request.form.get('Region'))
+
+#         new_data_scaled = standard_scaler.transform(
+#             [[Temperature, RH, Ws, Rain, FFMC, DMC, ISI, Classes, Region]]
+#         )
+#         result = ridge_model.predict(new_data_scaled)
+
+#         return render_template('home.html', result=result[0])
+#     else:
+#         return render_template('home.html')
+
+# if __name__ == "__main__":
+#     # Run the same WSGI object in local dev
+#     application.run(host="0.0.0.0")
+
+
 import pickle
 from flask import Flask, request, jsonify, render_template
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-# WSGI application object – name MUST be "application"
+# WSGI callable – Elastic Beanstalk will use THIS
 application = Flask(__name__)
+app = application
 
-## import ridge regressor model and standard scaler pickle
+# Load model and scaler
 ridge_model = pickle.load(open('models/ridge.pkl', 'rb'))
 standard_scaler = pickle.load(open('models/scaler.pkl', 'rb'))
 
-## Route for home page
-@application.route('/')
+# Home page
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@application.route('/predictdata', methods=['GET', 'POST'])
+# Prediction page
+@app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method == 'POST':
         Temperature = float(request.form.get('Temperature'))
@@ -79,9 +126,9 @@ def predict_datapoint():
         result = ridge_model.predict(new_data_scaled)
 
         return render_template('home.html', result=result[0])
-    else:
-        return render_template('home.html')
 
+    return render_template('home.html')
+
+# Only for local run (not used by EB)
 if __name__ == "__main__":
-    # Run the same WSGI object in local dev
-    application.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000, debug=True)
